a50e6b7f5f49f01ff9ace6aaadf37dd97778dfec
bhavana083 <bhavanavgowda08@gmail.com>
2025-01-13 11:03:01 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,21 @@
+var name ="Bhavana v"
+var age="23"
+console.log(name)
+console.log(age)
+
+let name1="pratik sushmitha"
+let place="Thailand"
+console.log(name1)
+console.log(place)
+
+var firstname="pratik " ,lastname="sarkar", age= "23", martial="married", country="Uganda";
+console.log(firstname,lastname,age,martial,country)
+
+var firstname="bhavana"
+var lastname="gowda"
+age="23"
+country="india"
+console.log(firstname)
+console.log(lastname)
+console.log(age)
+console.log(country)
